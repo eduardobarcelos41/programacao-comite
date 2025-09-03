38f644bbe94e399cf4abd8b0937cb59ff92ac9c4
eduardobarcelos41 <eduardo.barcelos.41.sb@gmail.com>
2025-09-02 21:51:06 -0300

    criação do programa de tarefas

---
+++ tarefas.py
@@ -0,0 +1,9 @@
+# calculando o dobro do numero informado
+#numero_1 = int(input("informe um numero inteiro ->"))
+#print("o dobro do numero informado é ->",numero_1 * 2)
+
+# calculando a area do quadrado 
+#lado = float(input("informe o valor do lado do quadrado ->"))
+#print("A area do quadrado é ->",lado * lado)
+
+# Calcular a idade em meses e dias 
