6e07ed8748815f099cc43c469176149e91265fcc
eduardobarcelos41 <eduardo.barcelos.41.sb@gmail.com>
2025-09-09 22:00:36 -0300

    exercicios de condicional

---
+++ Condicionais/maior_100.py
@@ -0,0 +1,7 @@
+valor_1 = float(input("informe o primeiro valor ->"))
+valor_2 = float(input("informe o segundo valor ->"))
+
+if valor_1 + valor_2 > 100:
+    print("A soma dos valores é maior que 100")
+else:
+    print("A soma dos valores é menor que 100")
